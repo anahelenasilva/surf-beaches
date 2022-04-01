466793516d4e4c1eb5ba0654509cf59e2331da68
Ana Helena <anahelenarp@hotmail.com>
2022-04-01 08:18:23 -0300

    test: return an empty list the beach array is empty

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -2,11 +2,12 @@ import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 import { Beach, BeachPosition, Forecast } from '../forecast';
 
+jest.mock('@src/clients/stormGlass');
+
 describe('Forecast Service', () => {
+	const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
 	it('should return the forecast for a list of beaches', async () => {
-		StormGlass.prototype.fetchPoints = jest
-			.fn()
-			.mockResolvedValue(stormGlassNormalizedResponseFixture);
+		mockedStormGlassService.fetchPoints.mockResolvedValue(stormGlassNormalizedResponseFixture);
 
 		const beaches: Beach[] = [
 			{
@@ -79,8 +80,14 @@ describe('Forecast Service', () => {
 				],
 			},
 		];
-		const forecast = new Forecast(new StormGlass());
+		const forecast = new Forecast(mockedStormGlassService);
 		const beachesWithRating = await forecast.processForecastForBeaches(beaches);
 		expect(beachesWithRating).toEqual(expectedResponse);
+	});
+
+	it('should return an empty list the beach array is empty', async () => {
+		const forecast = new Forecast();
+		const response = await forecast.processForecastForBeaches([]);
+		expect(response).toEqual([]);
 	});
 });
